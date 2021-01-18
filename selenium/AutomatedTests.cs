25d3fd0fdb713df92d28d92392f6e7ec043dcfbc
Pawel Lelental <p.lelental@gmail.com>
2021-01-18 20:51:13 +0100

    adjust address

--- selenium/AutomatedTests.cs
+++ selenium/AutomatedTests.cs
@@ -37,7 +37,7 @@ namespace Selenium
         public void TestShopProcess()
         {
             //Setting up and open the page
-            Driver.Navigate().GoToUrl("https://localhost/");
+            Driver.Navigate().GoToUrl("https://18.222.194.191/");
             Driver.Manage().Window.Maximize();
 
             //Add products to the cart
